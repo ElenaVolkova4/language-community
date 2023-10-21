cfcd86aeae7377c7233dd14d30e6c42ba22aaf4d
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2023-10-21 19:46:33 +0300

    work with mobile menu

---
+++ assets/scripts/index.js
@@ -0,0 +1,45 @@
+// работа кнопки меню
+const menuBtn = document.querySelector(".menu-mobile__btn");
+const mobileMenu = document.querySelector(".menu-mobile__list");
+const menuLinks = document.querySelectorAll(".menu__link");
+const menuBtnClose = document.querySelector(".menu-mobile__btn-close");
+
+// открытие меню
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("hidden");
+  mobileMenu.classList.add("show");
+});
+
+// закрытие меню при нажатии на ссылку в меню
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("show");
+    mobileMenu.classList.add("hidden");
+  });
+});
+
+// закрытие меню при клике на крестик
+menuBtnClose.addEventListener("click", () => {
+  mobileMenu.classList.remove("show");
+  mobileMenu.classList.add("hidden");
+});
+
+// закрытие меню при клике на любую другую область
+// document.addEventListener("click", (e) => {
+//   console.log("f");
+//   if (!mobileMenu.contains(e.target && e.target !== menuBtn)) {
+//     mobileMenu.classList.remove("show");
+//     mobileMenu.classList.add("hidden");
+//   }
+// });
+
+// закрытие меню при клике на любую область за его пределами
+
+document.addEventListener("click", (event) => {
+  const target = event.target;
+  console.log("f");
+  if (!mobileMenu.contains(target) && target !== menuBtn) {
+    mobileMenu.classList.remove("show");
+    mobileMenu.classList.add("hidden");
+  }
+});
